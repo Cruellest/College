720a8d9f24611b1c2b054f338c7fcd3a68c7ea5d
Cruellest <gabriel81523642@gmail.com>
2023-08-08 06:59:16 -0400

    Testing

---
+++ 2 Semestre/LPOO (Java)/1007/main.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+package 2 Semestre.LPOO (Java).1007;
+
+public class main {
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Hello World!\n");
+        
+
+    }
+}
